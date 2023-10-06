6177e69877339c4d2110c6aed6a49fbfda495652
Camila-Ailen <gcamilaailen@gmail.com>
2023-10-06 09:48:57 -0300

    login breeze (se ve horrible)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Mail\ContactMailable;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +14,18 @@ use App\Http\Controllers\ProductoController;
 |
 */
 
-Route::get('/', HomeController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('productos', ProductoController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('contact', [ContactController::class, 'index'])
-    ->name('contact.index'); 
-Route::post('contact', [ContactController::class, 'store'])
-    ->name('contact.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+require __DIR__.'/auth.php';
