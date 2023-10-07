f63d776b706803f47989aba82ba68088d76928ec
Camila-Ailen <gcamilaailen@gmail.com>
2023-10-07 10:57:10 -0300

    al iniciar dirige al home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,11 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -15,3 +23,5 @@
         </div>
     </div>
 </x-app-layout>
+
+@endsection
