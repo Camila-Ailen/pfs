955e7cfba6364bc5a2381aa434c489ade1c18472
Camila-Ailen <gcamilaailen@gmail.com>
2023-10-20 11:36:22 -0300

    todo listo para iniciar roles y permisos

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Spatie\Permission\Traits\HasRoles;
+
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use PhpParser\Node\Stmt\Return_;
 
@@ -16,6 +18,7 @@ use App\Models\Address;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
